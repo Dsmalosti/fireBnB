6c4403f8db5e5f62f6f6eb6c5ad32d88d0a63343
Diogo Santana Malosti <dsmalosti@gmail.com>
2025-03-22 14:19:19 -0300

    Verificando se o número é par ou ímpar

---
+++ funcoes.py
@@ -0,0 +1,20 @@
+
+def verifica_numero(num):
+    if num % 2 == 0:
+        return 'par'
+    else:
+        return 'ímpar'
+
+
+#testando a função verifica número
+def teste_numeros_impares_positivos():
+    assert verifica_numero(243) == 'ímpar'
+
+def teste_numeros_impares_negativos():
+    assert verifica_numero(-77) == 'ímpar'
+
+def teste_numeros_pares_positivos():
+    assert verifica_numero(84) == 'par'
+
+def teste_numeros_pares_negativos():
+    assert verifica_numero(-456) == 'par'
